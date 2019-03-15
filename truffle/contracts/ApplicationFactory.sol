2610f1687d0b0c60c80c690913d3e6682f47ec09
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-03-15 12:44:48 +0100

    Updating smart contracts

--- truffle/contracts/ApplicationFactory.sol
+++ truffle/contracts/ApplicationFactory.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract ApplicationFactory {
-    Application[] applications;
+contract ApplicationList {
+    address[] applications;
     address manager;
     bool acceptingApplication = true;
 
@@ -19,20 +19,23 @@ contract ApplicationFactory {
         _;
     }
 
-    function addExistingApplication(Application newApplication) public {
-        applications.push(newApplication);
-    }
-
-   
-//    function createApplication(string memory _username, string memory _teamName) public applicationsOpen {
-//        Application newApplication = new Application(_username, _teamName, msg.sender);
-//        applications.push(newApplication);
-//    }
-
-    function getApplications() public view returns (Application[] memory) {
+    function getApplications() public view returns (address[] memory) {
         return applications;
     }
 
+    function register(address newApplication) public applicationsOpen {
+        require(isContract(newApplication));
+        applications.push(newApplication);
+    }
+    
+    function isContract(address _addr) private returns (bool isContract){
+        uint32 size;
+        assembly {
+            size := extcodesize(_addr)
+        }
+        return (size > 0);
+    }
+        
     function finalize() public restricted {
         acceptingApplication = false;
     }
@@ -42,30 +45,30 @@ contract ApplicationFactory {
 contract Application {
 
     address manager;
-    string username;
     string teamName;
+    uint32 teamSize;
 
     modifier restricted {
         require(manager == msg.sender, "Only manager can update!");
         _;
     }
 
-    constructor(string memory _username, string memory _teamName) public {
+    constructor(string memory _teamName, uint32 _teamSize) public {
         manager = msg.sender;
-        username = _username;
         teamName = _teamName;
+        teamSize = _teamSize;
     }
 
-    function updateApplication(string memory _username, string memory _teamName) public restricted  {
-        username = _username;
+    function updateApplication(string memory _teamName, uint32 _teamSize) public restricted  {
         teamName = _teamName;
+        teamSize = _teamSize;
     }
     
     function getApplication() public view returns (Application) {
         return this;
     }
     
-    function getApplicationData() public view returns (string memory, string memory, address) {
-        return (username, teamName, manager);
+    function getApplicationData() public view returns (string memory, uint32, address) {
+        return (teamName, teamSize, manager);
     }
 }
