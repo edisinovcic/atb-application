2c2a46888348a6f2bb7e4579a1a72b743dca00a3
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-04-02 22:47:13 +0200

    React fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import applications from './getAllApplications'
+import myFunc from './getAllApplications'
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
+    constructor(props){
+        super(props);
 
-    this.state = {
-      applications: [],
-    };
-  }
+        this.state = {
+            applications: [],
+        };
+    }
 
-  async componentDidMount() {
-    const applications = applications;
+    async componentDidMount() {
+        const applications = await myFunc();
 
-    this.setState( {
-      applications: applications,
-    } );
-  }
+        this.setState( {
+            applications: applications,
+        } );
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Applications in Around the Block hackaton</h2>
-        {
-          <div>
-          this.state.applications.map(application => (
-          <li> {application} </li>
-          ))
+    render() {
+        return (
+            <div>
+                <h2>Applications in Around the Block hackaton</h2>
+
+                <div>
+                    {this.state.applications.map(application => (
+                        <li> {application} </li>
+                    ))}
+                </div>
+
             </div>
-        }
-      </div>
-    );
-  }
-
+        );
+    }
 }
 
 export default App;
+
+
+
--- frontend/src/getAllApplications.js
+++ frontend/src/getAllApplications.js
@@ -1,18 +1,17 @@
+import web3 from './web3';
+
 let hackathon_data = require('../../blockchain/hackathonapplications/build/contracts/HackathonApplications.json');
 let hackathon_address = "0xf1c94665d2c4d98704F89C3719CF66FFE2c76DD7";
 
 let application_data = require('../../blockchain/application/build/contracts/Application');
 let wallet = '0x06D608c9f761e7503e8Fb8906cb1bA387c754104';
 
-let Web3 = require('web3');
-let provider = new Web3.providers.HttpProvider("http://localhost:8545");
-const web3 = new Web3(provider);
-
 let HackathonApplications = new web3.eth.Contract(hackathon_data.abi, hackathon_address);
 let Application = new web3.eth.Contract(application_data.abi);
 
+/*
 let application_list = [];
-HackathonApplications.methods.getApplications().call({from: wallet}, (error, application) => {
+export default HackathonApplications.methods.getApplications().call({from: wallet}, (error, application) => {
     application.forEach(function (element) {
         Application.options.address = element;
         Application.methods.getApplicationData().call({from: wallet}, (error, result) => {
@@ -20,6 +19,21 @@ HackathonApplications.methods.getApplications().call({from: wallet}, (error, app
             application_list.append(result);
         })
     });
+    return application_list;
 });
+*/
 
-export default application_list;
+const myFunc = async () => {
+    let application_list = [];
+    await HackathonApplications.methods.getApplications().call({from: wallet}, (error, application) => {
+        application.forEach(function (element) {
+            Application.options.address = element;
+            Application.methods.getApplicationData().call({from: wallet}, (error, result) => {
+                console.log(result);
+                application_list.append(result);
+            })
+        });
+    });
+}
+
+export default myFunc;
